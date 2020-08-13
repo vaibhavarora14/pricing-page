89fcb50aeb8b4d0c05b327dd0d4d7952003fb54c
Vaibhav <varora@catalystone.com>
2020-08-13 21:20:00 +0530

    test: 💍 WIP:: add price-card implementation as per design

--- src/components/price-card.js
+++ src/components/price-card.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import { head, largeFont, separator, mediumFont, smallFont, bottom } from './price-card.style';
 
-const PriceCard = () => {
+const defaultProps = {
+    data: {
+        planName: '',
+        qualifiedLeadsPerMonth: 0,
+        pricePerLiveTransfer: '',
+        totalQualifiedLeadPrice: '',
+        totalPlatformPrice: '',
+        finalPackagePrice: ''
+    }
+}
+
+const PriceCard = (incomingProps) => {
+    const props = {
+        ...defaultProps,
+        ...incomingProps
+    };
+
     return (
-        <></>
+        <>
+            <div style={head} data-testid="plan-name">{props.data.planName}</div>
+            <center>
+                <div style={largeFont} data-testid="price-per-live-transfer">
+                    {props.data.pricePerLiveTransfer}
+                </div>
+                <div style={smallFont}>Per Qualified lead</div>
+
+                <div style={separator}></div>
+
+                <div style={smallFont}>Qualified Leads Per Month</div>
+                <div
+                    style={mediumFont}
+                    data-testid="qualified-leads-per-month"
+                >{props.data.qualifiedLeadsPerMonth}</div>
+
+                <div style={separator}></div>
+
+                <div style={smallFont}>Platform Fee Per Month</div>
+                <div
+                    style={{ ...mediumFont, marginBottom: '1rem' }}
+                    data-testid="total-platform-price"
+                >{props.data.totalPlatformPrice}</div>
+
+                <div
+                    style={{ ...bottom, ...mediumFont, fontWeight: 600 }}
+                    data-testid="final-package-price"
+                >{props.data.finalPackagePrice}/mo</div>
+
+                {/* Button - Start your trial */}
+            </center>
+        </>
     );
 }
 
---
+++ src/components/price-card.style.js
@@ -0,0 +1,29 @@
+export const head = {
+    backgroundColor: '#5A6F90',
+    padding: '1rem',
+    color: 'white',
+    textTransform: 'capitalize',
+    marginBottom: '0.5rem'
+}
+
+export const bottom = { ...head }
+
+export const largeFont = {
+    fontSize: '2.3rem',
+    fontWeight: '600',
+    letterSpacing: '1px'
+}
+
+export const mediumFont = {
+    fontSize: '1.2rem',
+    fontWeight: '500',
+}
+
+export const smallFont = {
+    fontSize: '0.9rem'
+}
+
+export const separator = {
+    borderBottom: '2px dashed lightgrey',
+    margin: '1rem 3rem'
+};
