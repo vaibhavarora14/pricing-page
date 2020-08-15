7f1c9b798311654a6abec456aa8952f3fafc95c6
Vaibhav <varora@catalystone.com>
2020-08-15 13:02:11 +0530

    feat: 🎸 made tab component compatible to docs + tests

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 
       <Tabs>
         <Tab name='Tab 1'>Tab 1 Content</Tab>
-        <Tab name='Tab 2'>Tab 2 Content</Tab>
+        <Tab selected name='Tab 2'>Tab 2 Content</Tab>
         <Tab name='Tab 3'>Tab 3 Content</Tab>
       </Tabs>
     </>
--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -1,41 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Tabs = (props) => {
-    const [selectedIndex, setIndex] = useState('');
+const Tabs = (incomingProps) => {
+    const props = { ...incomingProps };
+    const [selectedIndex, setIndex] = useState(0);
+    const [children, setChildren] = useState(props.children);
 
-    const getTabs = () => {
-        return props.children.map(showTabContent);
+    const updateInsideTabsProp = (children) => {
+        const childrenClone = children.map(child => React.cloneElement(child, { insideTabs: true }));
+        setChildren(childrenClone);
     }
 
-    const showTabContent = (child, index) => {
-        let childClone = child;
-        if (selectedIndex === index) {
-            childClone = React.cloneElement(child, { show: true })
+    const findSelectedTabIndex = (children) => {
+        const isTabSelected = (reactElement) => reactElement.props.selected;
+
+        const childrenArray = React.Children.toArray(children);
+        return childrenArray.findIndex(isTabSelected);
+    }
+
+    useEffect(() => {
+        updateInsideTabsProp(props.children);
+
+        const index = findSelectedTabIndex(props.children);
+        if (index > -1) {
+            setIndex(index);
         }
+    }, [props.children]);
+
+    const getTabContent = (children) => {
+        return children.map(updateSelectedProp);
+    }
+
+    const updateSelectedProp = (child, index) => {
+        let childClone = child;
+
+        if (selectedIndex === index) {
+            childClone = React.cloneElement(child, { selected: true, key: index });
+            return childClone;
+        }
+
+        childClone = React.cloneElement(child, { selected: false, key: index });
         return childClone;
     }
 
     return (
         <>
-            {props.children.map((tab, index) =>
-                <span id={index} onClick={() => setIndex(index)}>
+            {children.map((tab, index) =>
+                <span key={index} onClick={() => setIndex(index)}>
                     {tab.props.name}
                 </span>
             )}
 
-            {getTabs().map(child => child)}
+            {getTabContent(children).map(child => child)}
         </>
     );
 }
 
 const tabDefaultProps = {
     name: '',
-    show: false
+    selected: true,
+    insideTabs: false
 }
+
 const Tab = (incomingProps) => {
     const props = { ...tabDefaultProps, ...incomingProps };
     return (
-        <>{props.show && props.children}</>
+        <>
+            {!props.insideTabs && <span>{props.name}</span>}
+            <div>{props.selected && props.children}</div>
+        </>
     );
 }
 
